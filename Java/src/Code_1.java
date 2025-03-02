8d4deb1dc120e031c742aa9b4fb70449373dfa38
Aditya Singh <adi465996@gmail.com>
2025-03-02 16:56:34 +0530

    We wrote first code in java with print statement

--- Java/src/Code_1.java
+++ Java/src/Code_1.java
@@ -1,3 +1,8 @@
 public class Code_1 {
-
+public static void main (String []args){
+System.out.println("This is first Code in java");
+/*As you see in above statement we called system method and
+ then inside system method we called print instruction with this
+ One can print output */
+}
 }
