74d64a23f19be590903a1391d5ad5725b157ee64
Aditya Singh <adi465996@gmail.com>
2025-03-02 18:09:54 +0530

    We added our for loop syntax in code 1

--- Java/src/Code_1.java
+++ Java/src/Code_1.java
@@ -5,14 +5,21 @@ System.out.println("This is first Code in java");
 /*As you see in above statement we called system method and
  then inside system method we called print instruction with this
  One can print output */
- int num;
+ int num,x,y;
  num=100;
+ y=20;
  System.out.println("The number is "+num);
  if(num>200){
     System.out.println("The number is greater than 200");
  }
     else{ 
  System.out.println("The number is less than 200");
+    }
+    
+    for(x=0;x<10;x++){
+System.out.println("The value of x is "+x);
+System.out.println("The value of Y is decreasing with interval of '2'="+y);
+y=y-2;
     }
 }
 }
