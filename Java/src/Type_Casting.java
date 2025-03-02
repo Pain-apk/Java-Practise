31c5368409ba960d3e2a87576422258a462e511a
Aditya Singh <adi465996@gmail.com>
2025-03-03 01:13:52 +0530

    Here we created a code to see type casting in java

---
+++ Java/src/Type_Casting.java
@@ -0,0 +1,22 @@
+public class Type_Casting {
+public static void main(String []args){
+    /* Here we will see type casting and explore some concepts practically */
+    //Automatic casting(Widening Casting)
+    int a;
+    byte b;
+    b=5;
+    a=b;
+    System.out.println(a);
+    //Manual Casting(Narrowing)
+    int c;
+    double d;s
+    d = 3.4;
+    c= (int) d;
+    System.out.println(c);
+
+
+
+
+
+}
+}
