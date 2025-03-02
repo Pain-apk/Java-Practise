0c4a6bf0522290c35f6865ee238d17dffefb8ffc
Aditya Singh <adi465996@gmail.com>
2025-03-03 00:49:41 +0530

    Added a print statement

--- Java/src/Code_1.java
+++ Java/src/Code_1.java
@@ -2,6 +2,7 @@ public class Code_1 {
 
 public static void main (String []args){
 System.out.println("This is first Code in java");
+System.out.println("We are gonna look some basic operations in java");
 /*As you see in above statement we called system method and
  then inside system method we called print instruction with this
  One can print output */
